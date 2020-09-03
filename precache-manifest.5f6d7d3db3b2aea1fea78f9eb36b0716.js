49426622927793e5b9adc2608fcd62401238dd1a
Alexei <61819719+Alexes012@users.noreply.github.com>
2020-09-03 16:35:46 +0300

    Updates

---
+++ precache-manifest.5f6d7d3db3b2aea1fea78f9eb36b0716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f735d2536468680caf4b54777cc537b",
+    "url": "/test-task-ozitag/index.html"
+  },
+  {
+    "revision": "814bf7fd4af5ab310ece",
+    "url": "/test-task-ozitag/static/css/main.d874aaed.chunk.css"
+  },
+  {
+    "revision": "8988132276f79e60c451",
+    "url": "/test-task-ozitag/static/js/2.c7f1750f.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/test-task-ozitag/static/js/2.c7f1750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814bf7fd4af5ab310ece",
+    "url": "/test-task-ozitag/static/js/main.5d29e929.chunk.js"
+  },
+  {
+    "revision": "906d54dccc49c5163ce8",
+    "url": "/test-task-ozitag/static/js/runtime-main.ec29f436.js"
+  }
+]);
